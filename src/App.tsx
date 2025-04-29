57e13a451b1abaa59e77ea2d17e12c9004f877d8
문재연 <jaeyeonmando@gmail.com>
2025-04-29 14:43:28 +0900

    [KW-93] feat: admin login page ui 구현 (#27)
    * KW-93/feat: admin login page ui 구현
    
    * KW-93/fix: admin login page ui 수정

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
 import TablePage from './pages/TablePage'
 import Dashboard from './pages/Dashboard'
+import AdminLogin from './pages/AdminLogin'
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path="/table" element={<TablePage />} />
           <Route path="/dashboard" element={<Dashboard/>} />
+          <Route path="/adminlogin" element={<AdminLogin/>} />
         </Routes>
       </Router>
     </>
