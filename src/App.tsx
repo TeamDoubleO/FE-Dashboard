3f2e72c17eace2474b8b3b6afd07415fe022ed93
MinJu Chai <108432112+judymoody59@users.noreply.github.com>
2025-04-30 10:38:41 +0900

    KW-229/feat: sidebar logic 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,28 @@
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 
-import TablePage from './pages/TablePage'
-import Dashboard from './pages/Dashboard'
-import AdminLogin from './pages/AdminLogin'
+import TablePage from './pages/TablePage';
+import Dashboard from './pages/Dashboard';
+import AdminLogin from './pages/AdminLogin';
 import AdminMyPage from './pages/AdminMypage';
 import IssueHistoryPage from './pages/IssueHistoryPage';
-import EntryHistoryPage from './pages/entryHistoryPage';
+import EntryHistoryPage from './pages/EntryHistoryPage';
 
 function App() {
-
   return (
-    <>
     <Router>
-        <Routes>
-          <Route path="/table" element={<TablePage />} />
-          <Route path="/dashboard" element={<Dashboard/>} />
-          <Route path="/admin/login" element={<AdminLogin/>} />
-          <Route path="/admin/mypage" element={<AdminMyPage/>} />
-          <Route path="/issuehistory" element={<IssueHistoryPage/>} />
-        </Routes>
-      </Router>
-    </>
-  )
+      <Routes>
+        {/* 첫 진입 시 /dashboard로 */}
+        <Route path="/" element={<Navigate to="/dashboard" />} />
+
+        <Route path="/table" element={<TablePage />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/admin/login" element={<AdminLogin />} />
+        <Route path="/admin/mypage" element={<AdminMyPage />} />
+        <Route path="/issuehistory" element={<IssueHistoryPage />} />
+        <Route path="/entryhistory" element={<EntryHistoryPage />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
