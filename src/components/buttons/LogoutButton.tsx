3975fdbfe0767698d64b3f7fdfd65491ba5cedcb
문재연 <jaeyeonmando@gmail.com>
2025-04-30 17:24:18 +0900

    KW-237/fix: fix header and sidebar (#34)

--- src/components/buttons/LogoutButton.tsx
+++ src/components/buttons/LogoutButton.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import "./css/LogoutButton.css";
 
 interface LogoutButtonProps {
@@ -6,10 +7,13 @@ interface LogoutButtonProps {
 }
 
 const LogoutButton: React.FC<LogoutButtonProps> = ({ onLogout }) => {
+  const navigate = useNavigate();
+
   const handleLogoutClick = () => {
     const confirmed = window.confirm("로그아웃하시겠습니까?");
     if (confirmed) {
       onLogout();
+      navigate("/admin/login");
     }
   };
 
