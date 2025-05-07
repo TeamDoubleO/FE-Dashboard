129311f1bb02cab734e845786c8c8bea2c1a6aba
문재연 <jaeyeonmando@gmail.com>
2025-05-07 14:20:09 +0900

    [KW-94] feat: connect login api (#37)
    * KW-94/feat: connect login api
    
    * KW-94/feat: Private-Route 일부 설정
    
    * fix: PrivateRoute에 중첩 라우팅 구조 적용
    
    ---------
    
    Co-authored-by: judymoody59 <jmj.chai@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -7,21 +7,29 @@ import AdminMyPage from './pages/AdminMypage';
 import IssueHistoryPage from './pages/IssueHistoryPage';
 import EntryHistoryPage from './pages/EntryHistoryPage';
 import IssueDetailPage from './pages/IssueDetailPage';
+import PrivateRoute from './contexts/PrivateRoute';
 
 function App() {
   return (
     <Router>
       <Routes>
-        {/* 첫 진입 시 /dashboard로 */}
-        <Route path="/" element={<Navigate to="/dashboard" />} />
+        {/* 로그인 안했을 경우 홈으로 접근 시 로그인 페이지로 이동 */}
+        <Route path="/" element={<Navigate to="/admin/login" replace />} />
 
-        <Route path="/table" element={<TablePage />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        {/* 로그인 페이지 */}
         <Route path="/admin/login" element={<AdminLogin />} />
-        <Route path="/admin/mypage" element={<AdminMyPage />} />
-        <Route path="/issuehistory" element={<IssueHistoryPage />} />
-        <Route path="/issuedetail/:requestId" element={<IssueDetailPage />} />
-        <Route path="/entryhistory" element={<EntryHistoryPage />} />
+
+        {/* 보호된 라우트 그룹 */}
+        <Route element={<PrivateRoute />}>
+          <Route>
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/table" element={<TablePage />} />
+          <Route path="/admin/mypage" element={<AdminMyPage />} />
+          <Route path="/issuehistory" element={<IssueHistoryPage />} />
+          <Route path="/issuedetail/:requestId" element={<IssueDetailPage />} />
+          <Route path="/entryhistory" element={<EntryHistoryPage />} />
+          </Route>
+        </Route>
       </Routes>
     </Router>
   );
