0660e1a6f92866998b2069e064df447f7fbce0c0
문재연 <jaeyeonmando@gmail.com>
2025-05-23 13:58:37 +0900

    KW-418/fix: 로딩 컴포넌트 키위 로고 이미지로 교체

--- src/components/loading/Loading.tsx
+++ src/components/loading/Loading.tsx
@@ -1,11 +1,15 @@
-import Lottie from "lottie-react";
-import glowLoading from "./glow-loading.json"; // 경로 정확히 확인
-import "./css/Loading.css";
+import React from 'react';
+import KEYWEloading from '../../assets/images/KEYWE-loading.png';
+import './css/Loading.css';
 
-const Loading = () => {
+const Loading: React.FC = () => {
   return (
-    <div className="loading-wrapper">
-      <Lottie animationData={glowLoading} loop />
+    <div className="loading-container">
+      <img
+        src={KEYWEloading}
+        alt="Loading"
+        className="loading-image spinning"
+      />
     </div>
   );
 };
