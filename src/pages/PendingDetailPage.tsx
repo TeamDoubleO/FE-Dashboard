4050cfa44ead3fdd66262e2d1710ad7339c55a6e
judymoody59 <jmj.chai@gmail.com>
2025-05-27 15:02:57 +0900

    KW-479/fix: 출입증 신청 상세 조회 페이지에 출입시간 table 추가

--- src/pages/PendingDetailPage.tsx
+++ src/pages/PendingDetailPage.tsx
@@ -33,34 +33,50 @@ const PendingDetailPage = () => {
             currentSidebarItem={breadCrumbInfo.currentSidebarItem}
         />
 
-          <div className="pending-detail-container">
+        <div className="pending-detail-container">
             <div className="pending-detail-title">출입증 발급 신청 내역 상세 조회</div>
             <DefaultTable 
                 tableTitles={pendingColumn} 
                 data={pending}
             />
             <br /><br />
+            <div className="pending-detail-title">출입 시간</div>
+                <div className="pending-detail-table-wrapper">
+                <table className="pending-detail-table">
+                    <tbody>
+                    <tr>
+                        <td className="pending-detail-table-label-cell">출입시작시간</td>
+                        <td>{data.startAt}</td>
+                    </tr>
+                    <tr>
+                        <td className="pending-detail-table-label-cell">출입마감시간</td>
+                        <td>{data.expiredAt}</td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+            <br /><br />
             <div className="pending-detail-title">신청자 상세 정보</div>
                 <div className="pending-detail-table-wrapper">
-                    <table className="pending-detail-table">
-                        <tbody>
-                        <tr>
-                            <td className="pending-detail-table-label-cell">환자명</td>
-                            <td>{data.patientName}</td>
-                        </tr>
-                        <tr>
-                            <td className="pending-detail-table-label-cell">신청자명</td>
-                            <td>{data.guardianName}</td>
-                        </tr>
-                        <tr>
-                            <td className="pending-detail-table-label-cell">신청자 연락처</td>
-                            <td>{data.guardianContact}</td>
-                        </tr>
-                        </tbody>
-                    </table>
+                <table className="pending-detail-table">
+                    <tbody>
+                    <tr>
+                        <td className="pending-detail-table-label-cell">환자명</td>
+                        <td>{data.patientName}</td>
+                    </tr>
+                    <tr>
+                        <td className="pending-detail-table-label-cell">신청자명</td>
+                        <td>{data.guardianName}</td>
+                    </tr>
+                    <tr>
+                        <td className="pending-detail-table-label-cell">신청자 연락처</td>
+                        <td>{data.guardianContact}</td>
+                    </tr>
+                    </tbody>
+                </table>
                 </div>
             </div>
-      </Layout>
+    </Layout>
     </>
   );
 };
