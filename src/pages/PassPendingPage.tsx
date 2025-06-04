73fd2e68a9939ea14a8e2fde0144402ae4c790aa
judymoody59 <jmj.chai@gmail.com>
2025-06-04 17:04:14 +0900

    KW-581/fix: expiredAt 오타 수정

--- src/pages/PassPendingPage.tsx
+++ src/pages/PassPendingPage.tsx
@@ -37,7 +37,7 @@ const PassPendingPage = () => {
         ...item,
         createdDt: item.createdDt?.replace('T', '  '),
         startAt: item.startAt?.replace('T', '  '),
-        expiredDt: item.expiredDt?.replace('T', '  '),
+        expiredAt: item.expiredAt?.replace('T', '  '),
       }));
       setPendingList(transformed);
       setTotalPages(data.totalPages);
