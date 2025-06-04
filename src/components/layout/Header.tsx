36ded7a9b35843255be6f875dadb104670bbe6f3
문재연 <jaeyeonmando@gmail.com>
2025-06-04 15:23:41 +0900

    KW-579/fix: 헤더  UI 수정 (#71)

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -2,26 +2,42 @@ import './css/Header.css';
 import Logo from '../../assets/images/KEYWE_logo-white.png';
 import ReusableButton from '../buttons/ReusableButton';
 import { useNavigate } from 'react-router-dom';
-
+import { useEffect, useState } from 'react';
+import { IoPersonCircleOutline } from 'react-icons/io5';
 import { adminLogout } from '../../apis/loginApi';
+import { fetchAdminData } from '../../apis/adminApi';
 
 const Header = () => {
   const navigate = useNavigate();
+  const [hospitalName, setHospitalName] = useState<string>('');
+
+  useEffect(() => {
+    const loadAdminInfo = async () => {
+      try {
+        const data = await fetchAdminData();
+        setHospitalName(data.affiliation);
+      } catch (err) {
+        console.error('병원명 로딩 실패:', err);
+      }
+    };
+
+    loadAdminInfo();
+  }, []);
 
   const handleLogout = async () => {
-    const confirmed = window.confirm("로그아웃 하시겠습니까?");
+    const confirmed = window.confirm('로그아웃 하시겠습니까?');
     if (!confirmed) return;
 
     try {
       await adminLogout();
       localStorage.clear();
-      navigate("/admin/login");
+      navigate('/admin/login');
     } catch (err: any) {
       localStorage.clear();
-      const message = err?.message ?? "로그아웃 실패";
-      alert(message); 
-      console.warn("관리자 로그아웃 실패:", err);
-      navigate("/admin/login");
+      const message = err?.message ?? '로그아웃 실패';
+      alert(message);
+      console.warn('관리자 로그아웃 실패:', err);
+      navigate('/admin/login');
     }
   };
 
@@ -34,6 +50,14 @@ const Header = () => {
           className="header-logo-image"
           onClick={() => navigate('/dashboardstats')}
         />
+      </div>
+
+      <div className="header-user-wrapper">
+        <IoPersonCircleOutline className="header-user-icon" />
+        <div className="header-user-text">
+          <div>{hospitalName}</div>
+          <div className="header-user-subtext">관리자님 안녕하세요</div>
+        </div>
       </div>
 
       <div className="header-logout-wrapper">
