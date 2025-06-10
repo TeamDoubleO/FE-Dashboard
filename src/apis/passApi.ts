6272e0d50555d2345e04f9f6da23a4451cc4aaac
judymoody59 <jmj.chai@gmail.com>
2025-06-10 19:25:08 +0900

    KW-635/fix: pass API 함수 엔드포인트 변경

--- src/apis/passApi.ts
+++ src/apis/passApi.ts
@@ -4,7 +4,7 @@ import axiosWithAuthorization from "../contexts/axiosWithAuthorization";
 export const fetchEntryPassLog = async (page: number) => {
     try {
         const res = await axiosWithAuthorization.get(`/pass-logs/enter?page=${page}`);
-        console.log("출입 내역 조회:", res.data);
+        console.log("---> 출입 내역 조회:", res.data);
         return res.data.data;
     } catch (error) {
         console.log("출입 내역 조회 오류:", error); 
@@ -27,7 +27,7 @@ export const fetchIssuedPassLog = async (page: number) => {
 // 출입증 신청 요청 목록 조회
 export const fetchPassPending = async (page: number) => {
     try {
-        const res = await axiosWithAuthorization.get(`/pass-logs/pending?page=${page}`);
+        const res = await axiosWithAuthorization.get(`/admin-passes/pending?page=${page}`);
         console.log("출입증 신청 요청 목록 조회:", res.data);
         return res.data.data;
     } catch (error) {
@@ -39,7 +39,7 @@ export const fetchPassPending = async (page: number) => {
 // 보호자 신청 승인/거절
 export const reviewPass = async (passId: number, issuanceStatus: "ISSUED" | "REJECTED") => {
     try {
-        const res = await axiosWithAuthorization.post(`/pass-logs`, {
+        const res = await axiosWithAuthorization.post(`/admin-passes/approve`, {
             passId,
             issuanceStatus,
         });
