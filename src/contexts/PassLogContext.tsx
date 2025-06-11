3d2598d35d4f7664fa024fca631635477e6fb825
judymoody59 <jmj.chai@gmail.com>
2025-06-11 15:47:02 +0900

    KW-653/fix: 출입 로그 서비스 정상일 때 로그인 후 Warning 렌더링 오류 수정

--- src/contexts/PassLogContext.tsx
+++ src/contexts/PassLogContext.tsx
@@ -3,27 +3,34 @@ import { fetchEntryPassLog } from '../apis/passApi';
 
 export interface PassLogContextType {
   isPassLogAvailable: boolean;
+  hasCheckedAvailability: boolean; 
   checkAccessLogAvailability: () => Promise<boolean>;  
   setIsPassLogAvailable: React.Dispatch<React.SetStateAction<boolean>>;
+  setHasCheckedAvailability: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
 export const PassLogContext = createContext<PassLogContextType>({
   isPassLogAvailable: true, 
+  hasCheckedAvailability: false, 
   checkAccessLogAvailability: async () => false,
   setIsPassLogAvailable: () => {},
+  setHasCheckedAvailability: () => {},
 });
 
 export const PassLogProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   const [isPassLogAvailable, setIsPassLogAvailable] = useState(false);
+  const [hasCheckedAvailability, setHasCheckedAvailability] = useState(false); 
 
   const checkAccessLogAvailability = async (): Promise<boolean> => {
     try {
       await fetchEntryPassLog(0);
       setIsPassLogAvailable(true);
+      setHasCheckedAvailability(true);
       return true;
     } catch (error) {
       console.error('<---출입 로그 사용 불가--->:', error);
       setIsPassLogAvailable(false);
+      setHasCheckedAvailability(true);
       return false;
     }
   };
@@ -34,12 +41,12 @@ export const PassLogProvider: React.FC<{ children: React.ReactNode }> = ({ child
     if (reloaded === 'true') {
       console.log('새로고침 후 checkAccessLogAvailability 실행');
       checkAccessLogAvailability();
-      // sessionStorage.removeItem('reloaded');
+      sessionStorage.removeItem('reloaded');
     }
 
     const intervalId = setInterval(() => {
       checkAccessLogAvailability();
-    }, 20000);
+    }, 5000);
 
     const handleStorageChange = (event: StorageEvent) => {
       if (event.key === 'accessToken') {
@@ -63,8 +70,10 @@ export const PassLogProvider: React.FC<{ children: React.ReactNode }> = ({ child
     <PassLogContext.Provider
       value={{ 
         isPassLogAvailable, 
+        hasCheckedAvailability,    
         checkAccessLogAvailability,
         setIsPassLogAvailable, 
+        setHasCheckedAvailability,
       }}>
       {children}
     </PassLogContext.Provider>
